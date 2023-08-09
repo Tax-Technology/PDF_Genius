a23aae4c2f02d7bab5c5638bbc8833d6bd369e20
Tax-Technology <138007036+Tax-Technology@users.noreply.github.com>
2023-08-09 12:08:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
 from langchain.chains.question_answering import load_qa_chain
 
-# Initialize OpenAI API key variable
-openai_api_key = ""
-
 class CharacterTextSplitter:
     def __init__(self, separator="\n", chunk_size=800, chunk_overlap=200, length_function=len):
         self.separator = separator
@@ -39,18 +36,26 @@ if __name__ == "__main__":
     # Allow user to input their OpenAI API key
     openai_api_key = st.text_input("Enter your OpenAI API key:")
 
-    # Check if the API key is provided
+    # Check if the API key is provided and valid
     if openai_api_key:
-        llm = OpenAIEmbeddings(openai_api_key=openai_api_key, temperature=0)
+        try:
+            llm = OpenAIEmbeddings(openai_api_key=openai_api_key, temperature=0)
+        except Exception as e:
+            st.error("Error: Invalid OpenAI API key. Please provide a valid key.")
+            st.stop()
 
         pdf_file = st.file_uploader("Choose a PDF file", type="pdf")
 
         if pdf_file is not None:
-            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-                tmp_file.write(pdf_file.read())
-                pdf_path = tmp_file.name
-                reader = PdfReader(pdf_path)
-                pages = reader.pages
+            try:
+                with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                    tmp_file.write(pdf_file.read())
+                    pdf_path = tmp_file.name
+                    reader = PdfReader(pdf_path)
+                    pages = reader.pages
+            except Exception as e:
+                st.error("Error: Invalid PDF file. Please provide a valid PDF.")
+                st.stop()
 
             page_selection = st.radio("Page selection", ["Single page", "Page range", "Overall Summary", "Question"])
 
