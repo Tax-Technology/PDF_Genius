67ff23db070cbb171d407bcc0e79a864418015e0
Tax-Technology <138007036+Tax-Technology@users.noreply.github.com>
2023-08-09 14:18:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,12 @@ import openai
 import tempfile
 from PyPDF2 import PdfReader
 
-# Set your OpenAI API key
-openai_api_key = "YOUR_OPENAI_API_KEY"
-
-# Initialize the OpenAI API client
-openai.api_key = openai_api_key
-
 def summarize(text, max_tokens=150):
     response = openai.Completion.create(
         engine="davinci",
         prompt=text,
-        max_tokens=max_tokens
+        max_tokens=max_tokens,
+        temperature=0.0  # Set temperature to 0 for deterministic output
     )
     return response.choices[0].text.strip()
 
@@ -22,7 +17,8 @@ def answer_question(pages, question):
     response = openai.Answer.create(
         model="davinci",
         question=question,
-        documents=[context]
+        documents=[context],
+        temperature=0.0  # Set temperature to 0 for deterministic output
     )
     return response.answers[0]
 
@@ -31,46 +27,51 @@ if __name__ == "__main__":
 
     pdf_file = st.file_uploader("Choose a PDF file", type="pdf")
 
-    if pdf_file is not None:
-        try:
-            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-                tmp_file.write(pdf_file.read())
-                pdf_path = tmp_file.name
-                reader = PdfReader(pdf_path)
-                pages = reader.pages
-        except Exception as e:
-            st.error("Error: Invalid PDF file. Please provide a valid PDF.")
-            st.stop()
+    openai_api_key = st.text_input("Enter your OpenAI API key:")
+    if openai_api_key:
+        # Initialize the OpenAI API client
+        openai.api_key = openai_api_key
 
-        page_selection = st.radio("Page selection", ["Single page", "Page range", "Overall Summary", "Question"])
+        if pdf_file is not None:
+            try:
+                with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                    tmp_file.write(pdf_file.read())
+                    pdf_path = tmp_file.name
+                    reader = PdfReader(pdf_path)
+                    pages = reader.pages
+            except Exception as e:
+                st.error("Error: Invalid PDF file. Please provide a valid PDF.")
+                st.stop()
 
-        if page_selection == "Single page":
-            page_number = st.number_input("Enter page number", min_value=1, max_value=len(pages), value=1, step=1)
-            page_content = pages[page_number - 1].extract_text()
-            summary = summarize(page_content)
-            st.subheader("Summary")
-            st.write(summary)
-            st.button("Clear")
+            page_selection = st.radio("Page selection", ["Single page", "Page range", "Overall Summary", "Question"])
 
-        elif page_selection == "Page range":
-            start_page = st.number_input("Enter start page", min_value=1, max_value=len(pages), value=1, step=1)
-            end_page = st.number_input("Enter end page", min_value=start_page, max_value=len(pages), value=start_page, step=1)
+            if page_selection == "Single page":
+                page_number = st.number_input("Enter page number", min_value=1, max_value=len(pages), value=1, step=1)
+                page_content = pages[page_number - 1].extract_text()
+                summary = summarize(page_content)
+                st.subheader("Summary")
+                st.write(summary)
+                st.button("Clear")
 
-            combined_content = " ".join([page.extract_text() for page in pages[start_page - 1:end_page]])
-            summary = summarize(combined_content)
-            st.subheader("Summary")
-            st.write(summary)
-            st.button("Clear")
+            elif page_selection == "Page range":
+                start_page = st.number_input("Enter start page", min_value=1, max_value=len(pages), value=1, step=1)
+                end_page = st.number_input("Enter end page", min_value=start_page, max_value=len(pages), value=start_page, step=1)
 
-        elif page_selection == "Overall Summary":
-            overall_content = " ".join([page.extract_text() for page in pages])
-            summary = summarize(overall_content)
-            st.subheader("Summary")
-            st.write(summary)
-            st.button("Clear")
+                combined_content = " ".join([page.extract_text() for page in pages[start_page - 1:end_page]])
+                summary = summarize(combined_content)
+                st.subheader("Summary")
+                st.write(summary)
+                st.button("Clear")
 
-        elif page_selection == "Question":
-            question = st.text_input("Enter your question")
-            answer = answer_question(pages, question)
-            st.write("Answer:", answer)
-            st.button("Clear")
+            elif page_selection == "Overall Summary":
+                overall_content = " ".join([page.extract_text() for page in pages])
+                summary = summarize(overall_content)
+                st.subheader("Summary")
+                st.write(summary)
+                st.button("Clear")
+
+            elif page_selection == "Question":
+                question = st.text_input("Enter your question")
+                answer = answer_question(pages, question)
+                st.write("Answer:", answer)
+                st.button("Clear")
